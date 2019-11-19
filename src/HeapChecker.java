39004708e3ddc0d005264c955472452c682eeea2
brian <briancfay1@gmail.com>
2019-11-19 14:22:13 -0500

    Given files and javadoc template added

---
+++ src/HeapChecker.java
@@ -0,0 +1,28 @@
+public class HeapChecker {
+
+
+    /**
+     * Searches for a book with a matching title.
+     * @param myTitle The title we're searching for.
+     * @return True if a matching book is found.
+     */
+    boolean addEltTester(IHeap hOrig, int elt, IBinTree hAdded) {
+
+//    ...code to compare hOrig and hAdded around the addition of elt as appropriate...
+    return true;
+    }
+
+
+    /**
+     * Searches for a book with a matching title.
+     * @param myTitle The title we're searching for.
+     * @return True if a matching book is found.
+     */
+    boolean remMinEltTester(IHeap hOrig, IBinTree hRemoved) {
+
+//    ...code to compare hOrig and hRemoved as appropriate...
+        return true;
+    }
+
+
+}
